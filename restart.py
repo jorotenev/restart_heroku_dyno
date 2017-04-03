b0eac581dfef168c446ddbc0ce16381bd6c3f7e3
Georgi Tenev <chipsanbg@gmail.com>
2017-04-03 12:28:06 +0200

    added the authorization token to the header

--- restart.py
+++ restart.py
@@ -7,18 +7,19 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-# reference https://devcenter.heroku.com/articles/platform-api-reference#dyno-restart
-heroku_api_url_template = "https://api.heroku.com/apps/{app_id}/dynos/{dyno_name}"
-heroku_headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/vnd.heroku+json; version=3"
-}
-heroku_successful_http_code = 202
-
 token_api = getenv("TOKEN_API")
 dyno_name = getenv("DYNO_NAME")
 app_id = getenv("APP_ID")
 assert token_api and dyno_name and app_id, "Mandatory env variables not set"
+
+# reference https://devcenter.heroku.com/articles/platform-api-reference#dyno-restart
+heroku_api_url_template = "https://api.heroku.com/apps/{app_id}/dynos/{dyno_name}"
+heroku_headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/vnd.heroku+json; version=3",
+    "Authorization": "Bearer {token}".format(token=token_api)
+}
+heroku_successful_http_code = 202
 
 try:
     # construct the url for the request
@@ -30,7 +31,7 @@ try:
     response = delete(url, headers=heroku_headers)
     # see if it was successful
     if response.status_code == heroku_successful_http_code:
-        logging.info("API call for restart of dyno [{dyno}] successful")
+        logging.info("API call for restart of dyno [{dyno}] successful".format(dyno=dyno_name))
         exit(0)
     else:
         error_msg = "API call failed with code {status_code}. Msg: {err_text}".format(status_code=response.status_code,
